95a0b6b25f6cf0aa4ed5daafc6caaecf00f34647
Adam Gierscher <a.gierscher@live.ca>
2015-01-08 19:23:43 -0500

    Real Website

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,1 +1,5 @@
-console.log('This would be the main JS file.');
+function Layout()
+{
+    var c = document.getElementById("contact");
+    c.children[1].style.width = String(c.offsetWidth - 300) + "px";
+}
