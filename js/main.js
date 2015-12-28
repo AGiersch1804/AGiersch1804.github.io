9f78df74f0b76ff5a2208c88f9e9a00959ed986e
Adam Gierscher <a.gierscher@live.ca>
2015-12-28 15:28:16 -0500

    Saving before wiping PC

---
+++ js/main.js
@@ -0,0 +1,32 @@
+﻿var app = angular.module('agApp', ['ngMaterial']);
+
+app.controller('agController', function ($scope) {
+    console.log('IM RUNNING');
+
+    $scope.schoolStuff = [{
+        'subject': 'Math',
+        'classes': [
+            'Algebra',
+            'Linear Algebra Part 1',
+            'Linear Algebra Part 2',
+            'Calculus Part 1',
+            'Calculus Part 2',
+            'Statistics Part 1'
+        ]
+    }, {
+        'subject': 'Computer Science',
+        'classes': [
+            'Object Oriented Programming',
+            'Functional Programming',
+            'Data Abstraction',
+            'Computational Logic'
+        ]
+    }];
+
+});
+
+app.config(function ($mdThemingProvider) {
+    $mdThemingProvider.theme('default')
+        .primaryPalette('orange')
+        .accentPalette('blue');
+});
