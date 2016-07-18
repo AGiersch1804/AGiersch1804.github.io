4150489fe40d20c39a921a9d7b315e0432396aa6
Adam Gierscher <a.gierscher@live.ca>
2016-07-18 11:16:18 -0400

    Updated work experience

---
+++ main.js
@@ -0,0 +1,10 @@
+﻿angular
+    .module('agApp', ['ngMaterial'])
+    .config(function ($mdThemingProvider) {
+        $mdThemingProvider
+            .theme('default')
+            .primaryPalette('orange')
+            .accentPalette('cyan')
+            .warnPalette('red')
+            .backgroundPalette('grey');
+    });
